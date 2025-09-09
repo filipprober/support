4e6983aff367fecd5a24968e4e8b15c00c7a9f0d
filip-codes <filip@bitecorn.com>
2025-09-09 18:21:25 +0200

    chore: updated String.reverse() tests, added example for String.reverse()

---
+++ examples/string/reverse.dart
@@ -0,0 +1,10 @@
+import 'package:support/string.dart';
+
+///
+/// Example of using the `reverse` extension method on String.
+///
+void main() {
+  print("raBooF".reverse());      // Output: "FooBar"
+  print("őtüzsineT".reverse());   // Output: "Teniszütő"
+  print("☆etyBitluM❤".reverse()); // Output: "❤MultiByte☆"
+}
--- test/string_test.dart
+++ test/string_test.dart
@@ -2,21 +2,20 @@ import 'package:support/string.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('Support/String', () {
-    test('after', () {
-      expect("nah", "hannah".after("han"));
-      expect("nah", "hannah".after("n"));
-      expect("nah", "ééé hannah".after("han"));
-      expect("hannah", "hannah".after("xxxx"));
-      expect("hannah", "hannah".after(""));
-      expect("nah", "han0nah".after("0"));
-      expect("nah", "han0nah".after(0));
-      expect("nah", "han2nah".after(2));
-    });
+  test('after', () {
+    expect("nah", "hannah".after("han"));
+    expect("nah", "hannah".after("n"));
+    expect("nah", "ééé hannah".after("han"));
+    expect("hannah", "hannah".after("xxxx"));
+    expect("hannah", "hannah".after(""));
+    expect("nah", "han0nah".after("0"));
+    expect("nah", "han0nah".after(0));
+    expect("nah", "han2nah".after(2));
+  });
 
-    test('can reverse strings', () {
-      expect("support".reverse(), "troppus");
-      expect("string".reverse(), "gnirts");
-    });
+  test('reverse', () {
+    expect("FooBar", "raBooF".reverse());
+    expect("Teniszütő", "őtüzsineT".reverse());
+    expect("❤MultiByte☆", "☆etyBitluM❤".reverse());
   });
 }
