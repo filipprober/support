e7a3e5d31cd5ecddaba12a6c9c974a66187ec432
filip-codes <filip@bitecorn.com>
2025-09-10 11:15:13 +0200

    docs: StringExtension documentation improvements

--- lib/src/extensions/string.dart
+++ lib/src/extensions/string.dart
@@ -1,75 +1,132 @@
 extension StringExtension on String {
+  ///
   /// Return the remainder of a string after the first occurrence of a given value.
+  ///
+  /// **Parameters**
+  /// * [search] - [String] | [int]
+  ///
+  /// **Throws**
+  /// * [TypeError]
+  ///
   String after(dynamic search) {
     if (search is! String && search is! int) {
-      throw ArgumentError('Search must be a String or an int');
+      throw TypeError;
     }
 
     search = search.toString();
 
-    if (search.isEmpty) return this;
+    if (search.isEmpty) {
+      return this;
+    }
 
     int index = indexOf(search);
-    if (index == -1) return this;
+    if (index == -1) {
+      return this;
+    }
 
     return substring(index + search.length);
   }
 
+  ///
   /// Return the remainder of a string after the last occurrence of a given value.
+  ///
+  /// **Parameters**
+  /// * [search] - [String] | [int]
+  ///
+  /// **Throws**
+  /// * [TypeError]
+  ///
   String afterLast(dynamic search) {
     if (search is! String && search is! int) {
-      throw ArgumentError('Search must be a String or an int');
+      throw TypeError;
     }
 
     search = search.toString();
 
-    if (search.isEmpty) return this;
+    if (search.isEmpty) {
+      return this;
+    }
 
     int index = lastIndexOf(search);
-    if (index == -1) return this;
+    if (index == -1) {
+      return this;
+    }
 
     return substring(index + search.length);
   }
 
+  ///
   /// Get the portion of a string before the first occurrence of a given value.
+  ///
+  /// **Parameters**
+  /// * [search] - [String] | [int]
+  ///
+  /// **Throws**
+  /// * [TypeError]
+  ///
   String before(dynamic search) {
     if (search is! String && search is! int) {
-      throw ArgumentError('Search must be a String or an int');
+      throw TypeError;
     }
 
     search = search.toString();
 
-    if (search.isEmpty) return this;
+    if (search.isEmpty) {
+      return this;
+    }
 
     int index = indexOf(search);
-    if (index == -1) return this;
+    if (index == -1) {
+      return this;
+    }
 
     return substring(0, index);
   }
 
+  ///
   /// Get the portion of a string before the last occurrence of a given value.
+  ///
+  /// **Parameters**
+  /// * [search] - [String] | [int]
+  ///
+  /// **Throws**
+  /// * [TypeError]
+  ///
   String beforeLast(dynamic search) {
     if (search is! String && search is! int) {
-      throw ArgumentError('Search must be a String or an int');
+      throw TypeError;
     }
 
     search = search.toString();
 
-    if (search.isEmpty) return this;
+    if (search.isEmpty) {
+      return this;
+    }
 
     int index = lastIndexOf(search);
-    if (index == -1) return this;
+    if (index == -1) {
+      return this;
+    }
 
     return substring(0, index);
   }
 
+  ///
   /// Get the portion of a string between two given values.
+  ///
+  /// **Parameters**
+  /// * [from] - [String] | [int]
+  /// * [to] - [String] | [int]
+  ///
+  /// **Throws**
+  /// * [TypeError]
+  ///
   String between(dynamic from, dynamic to) {
     if (from is! String && from is! int) {
-      throw ArgumentError('From must be a String or an int');
+      throw TypeError;
     }
     if (to is! String && to is! int) {
-      throw ArgumentError('To must be a String or an int');
+      throw TypeError;
     }
 
     from = from.toString();
@@ -82,13 +139,22 @@ extension StringExtension on String {
     return this.after(from).beforeLast(to);
   }
 
+  ///
   /// Get the smallest possible portion of a string between two given values.
+  ///
+  /// **Parameters**
+  /// * [from] - [String] | [int]
+  /// * [to] - [String] | [int]
+  ///
+  /// **Throws**
+  /// * [TypeError]
+  ///
   String betweenFirst(dynamic from, dynamic to) {
     if (from is! String && from is! int) {
-      throw ArgumentError('From must be a String or an int');
+      throw TypeError;
     }
     if (to is! String && to is! int) {
-      throw ArgumentError('To must be a String or an int');
+      throw TypeError;
     }
 
     from = from.toString();
@@ -101,7 +167,9 @@ extension StringExtension on String {
     return this.after(from).before(to);
   }
 
+  ///
   /// Convert the first character of the given string to lower-case.
+  ///
   String lcfirst() {
     if (isEmpty) {
       return this;
@@ -110,12 +178,35 @@ extension StringExtension on String {
     return lower()[0] + substring(1);
   }
 
+  ///
   /// Convert the given string to lower-case.
+  ///
   String lower() {
     return toLowerCase();
   }
 
+  /// Reverse the given string.
+  String reverse() {
+    return split('').reversed.join();
+  }
+
+  ///
+  /// Take the first or last [limit] characters of a string.
+  ///
+  /// **Parameters**
+  /// * [limit] - [int]
+  ///
+  String take(int limit) {
+    if (limit < 0) {
+      return substring(length + limit.clamp(-length, 0), length);
+    }
+
+    return substring(0, limit.clamp(0, length));
+  }
+
+  ///
   /// Make a string's first character uppercase.
+  ///
   String ucfirst() {
     if (isEmpty) {
       return this;
@@ -124,22 +215,10 @@ extension StringExtension on String {
     return upper()[0] + substring(1);
   }
 
+  ///
   /// Convert the given string to upper-case.
+  ///
   String upper() {
     return toUpperCase();
-  }
-
-  /// Reverse the given string.
-  String reverse() {
-    return split('').reversed.join();
-  }
-
-  /// Take the first or last {limit} characters of a string.
-  String take(int limit) {
-    if (limit < 0) {
-      return substring(length + limit.clamp(-length, 0), length);
-    }
-
-    return substring(0, limit.clamp(0, length));
   }
 }
